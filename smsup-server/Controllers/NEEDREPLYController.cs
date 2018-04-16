4cd60eac2499f40c573a01010b0869a20f9fcf73
qcmiao1998 <qcmiao@hotmail.com>
2018-04-16 20:48:48 +0800

    Fix Bugs

--- smsup-server/Controllers/NEEDREPLYController.cs
+++ smsup-server/Controllers/NEEDREPLYController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
 using Newtonsoft.Json.Linq;
 
@@ -10,27 +11,28 @@ namespace smsup_server.Controllers
 {
     public class NEEDREPLYController : ApiController
     {
-        public string Post(dynamic jsonr)
+        public HttpResponseMessage Post(dynamic jsonr)
         {
             string returnJson;
+            JObject jObject = new JObject();
             try
             {
                 string json = jsonr.ToString();
                 JObject jrow = JObject.Parse(json.ToString());
                 Data.Mongo.InsertNeedReply((string)jrow["phone_number"], (string)jrow["send_time"]);
-                returnJson = @"{
-                                ""code"": 0,
-                                ""msg"": ""成功""
-                            }";
+                //returnJson = @"{""code"": 0, ""msg"": ""成功""}";
+                jObject.Add("code", 0);
+                jObject.Add("msg", "成功");
             }
             catch (Exception)
             {
-                returnJson = @"{
-                                ""code"": 1,
-                                ""msg"": ""失败""
-                            }";
+                jObject.Add("code", 1);
+                jObject.Add("msg", "失败");
+                //returnJson = @"{""code"": 1,""msg"": ""失败""}";
             }
-            return returnJson;
+            returnJson = jObject.ToString().Replace("\r\n", "");
+            HttpResponseMessage result = new HttpResponseMessage { Content = new StringContent(returnJson, Encoding.GetEncoding("UTF-8"), "application/json") };
+            return result;
         }
     }
 }
--- smsup-server/Controllers/SMSUPController.cs
+++ smsup-server/Controllers/SMSUPController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Text;
 using System.Web.Http;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
@@ -11,10 +12,11 @@ namespace smsup_server.Controllers
 {
     public class SMSUPController : ApiController
     {
-        public string Post(dynamic jsonr)
+        public HttpResponseMessage Post(dynamic jsonr)
         {
             string returnJson;
             string json = jsonr.ToString();
+            JObject jObject = new JObject();
             try
             {
                 if (!json.StartsWith("[") || !json.EndsWith("]"))
@@ -28,20 +30,20 @@ namespace smsup_server.Controllers
                     Data.Mongo.InsertUpMessage((string)jrow["phone_number"], (string)jrow["content"], (string)jrow["send_time"]);
                     Data.Mongo.UpdateReplyStatus((string)jrow["phone_number"], (string)jrow["send_time"]);
                 }
-                returnJson = @"{
-                                ""code"": 0,
-                                ""msg"": ""成功""
-                            }";
+                //returnJson = @"{""code"": 0, ""msg"": ""成功""}";
+                jObject.Add("code", 0);
+                jObject.Add("msg", "成功");
             }
             catch (Exception)
             {
-                returnJson = @"{
-                                ""code"": 1,
-                                ""msg"": ""失败""
-                            }";
+                //returnJson = @"{""code"": 1,""msg"": ""失败""}";
+                jObject.Add("code", 1);
+                jObject.Add("msg", "失败");
             }
 
-            return returnJson;
+            returnJson = jObject.ToString().Replace("\r\n", "");
+            HttpResponseMessage result = new HttpResponseMessage { Content = new StringContent(returnJson, Encoding.GetEncoding("UTF-8"), "application/json") };
+            return result;
         }
     }
 }
